caa356af47872bd39286def31cbd6ee4988589f0
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-05-07 16:35:35 +0200

    header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>home</h1>
-</body>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Pasta Molisana</title>
+        <link rel="stylesheet" href="{{asset("css/app.css")}}">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <img src="{{asset("images/marchio-sito-test.png")}}" alt="">
+                <nav>
+                    <ul>
+                        <li class="active"><a href="#">home</a></li>
+                        <li><a href="#">prodotti</a></li>
+                        <li><a href="#">news</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </body>
 </html>
