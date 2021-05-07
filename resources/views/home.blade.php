73e132d202919e3a1267665fd020eb763590cfdf
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-05-07 18:07:42 +0200

    categorie tutte completate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,12 +21,32 @@
             </div>
         </header>
         <main>
-            
+            {{-- carte --}}
             <div class="container">
                 <div>
                     <h2>le lunghe</h2>
                     <div class="cards">
                         @foreach ($lunghe as $pasta)
+                            <div class="card">
+                                <img src="{{$pasta["src"]}}" alt="{{$pasta["titolo"]}}">
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                <div>
+                    <h2>le lunghe</h2>
+                    <div class="cards">
+                        @foreach ($corte as $pasta)
+                            <div class="card">
+                                <img src="{{$pasta["src"]}}" alt="{{$pasta["titolo"]}}">
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                <div>
+                    <h2>le cortissime</h2>
+                    <div class="cards">
+                        @foreach ($cortissime as $pasta)
                             <div class="card">
                                 <img src="{{$pasta["src"]}}" alt="{{$pasta["titolo"]}}">
                             </div>
