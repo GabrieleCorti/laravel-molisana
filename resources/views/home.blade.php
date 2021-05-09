6c4aa6456ee739262af7b66a215b56c05c0d2a6c
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-05-09 18:31:32 +0200

    fine footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,29 +55,64 @@
                 </div>
             </div>
         </main>
-        <footer class="container">
-            <div class="recapiti">
-                <div class="wrap">
-                    <img src="{{asset("images/marchio-sito-test.png")}}" alt="">
+        <footer>
+            <div class="container">
+                <div class="recapiti">
+                    <div class="wrap">
+                        <img src="{{asset("images/marchio-sito-test.png")}}" alt="">
+                    </div>
+                    <address class="info">
+                        <ul>
+                            <li>Ragione sociale: la Molisana S.P.A.</li>
+                            <li>sede legale: Contrada Colle Delle Api. 100/A</li>
+                            <li>86100-Campobasso</li>
+                            <li>Pec: lamolisana@pec</li>
+                            <li>Tel: 212321323231</li>
+                            <li>Fax: 231234232</li>
+                            <li>info@lamolisana.it (per segnalazioni degli utenti)</li>
+                            <li>comemrcale@lamolisana.it</li>
+                            <li>export@lamolisana.it</li>
+                            <li>numero verde: 800818081</li>
+                            <li>telefono 3801292455</li>
+                        </ul>
+                    </address>
                 </div>
-                <address class="info">
-                    <ul>
-                        <li>Ragione sociale: la Molisana S.P.A.</li>
-                        <li>sede legale: Contrada Colle Delle Api. 100/A</li>
-                        <li>86100-Campobasso</li>
-                        <li>Pec: lamolisana@pec</li>
-                        <li>Tel: 212321323231</li>
-                        <li>Fax: 231234232</li>
-                        <li>info@lamolisana.it</li>
-                        <li>comemrcale@lamolisana.it</li>
-                        <li>export@lamolisana.it</li>
-                        <li>numero verde: 800818081</li>
-                        <li>telefono 3801292455</li>
-                    </ul>
-                </address>
-            </div>
-            <div class="navFoot">
-
+                <div class="navFoot">
+                    <div class="listaLinkFooter">
+                        <h3>Pastficio</h3>
+                        <ul>
+                            <li><a href="#">Il PAstifico</a></li>
+                            <li><a href="#">Grano decorticato a pietra</a></li>
+                            <li><a href="#">Il Molise c'è</a></li>
+                            <li><a href="#">Filiera Integrata</a></li>
+                            <li><a href="#">100 anni di pasta</a></li>
+                            <li><a href="#">Sartoria della pasta</a></li>
+                            <li><a href="#">Spaghetto Quadrato</a></li>
+                            <li><a href="#">Le Persone</a></li>
+                        </ul>
+                    </div>
+                    <div class="listaLinkFooter">
+                        <h3>collezone da chef</h3>
+                        <ul>
+                            <li><a href="#">Collezioni da CHef</a></li>
+                            <li><a href="#">Grandi Cucine</a></li>
+                            <li><a href="#">Biologiche</a></li>
+                            <li><a href="#">Quadrate</a></li>
+                        </ul>
+                    </div>
+                    <div class="listaLinkFooter">
+                        <h3>Prodotti</h3>
+                        <ul>
+                            <li><a href="#">Le Classiche</a></li>
+                            <li><a href="#">Le Integrali</a></li>
+                            <li><a href="#">Le Speciali</a></li>
+                            <li><a href="#">Le Biologiche</a></li>
+                            <li><a href="#">Le Gluted-Free</a></li>
+                            <li><a href="#">Le Semole</a></li>
+                            <li><a href="#">Le Extra di Lusso</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </footer>
     </body>
