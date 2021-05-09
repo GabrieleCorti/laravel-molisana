bb18cdd80936936532b45e61e99d67777b6a0dbf
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-05-09 18:41:34 +0200

    creati componenti e inclusi in index

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,12 @@
+ <header>
+    <div class="container">
+        <img src="{{asset("images/marchio-sito-test.png")}}" alt="">
+        <nav>
+            <ul>
+                <li class="active"><a href="#">home</a></li>
+                <li><a href="#">prodotti</a></li>
+                <li><a href="#">news</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
